35fd2c92c5f26db11c22fe6c186835f3c5e31c22
Eugene Grady <grady.gene.thomas@gmail.com>
2019-10-01 17:02:47 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,10 +36,10 @@ end
 def hit?(card_total)
   prompt_user
   user_input = get_user_input
-  if user_input == "h"
+  if user_input == 's'
     card_total += deal_card
   elsif
-    user_input == "s"
+    user_input == 's'
     card_total
   else
     invalid_command
@@ -48,7 +48,7 @@ end
 
 def invalid_command
   puts "Please enter a valid command"
-  puts "Type 'h' to hit or 's' to stay"
+  prompt_user
   get_user_input
 end
 
